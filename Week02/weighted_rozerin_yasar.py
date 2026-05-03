e99ef65da5ffc8ab88d600e442f32c717d643bcd
Rozerin Yaşar <rozerinyasar54@gmail.com>
2026-05-03 14:23:53 +0300

    Refactor weighted_srs function for unique sampling

--- Week02/weighted_rozerin_yasar.py
+++ Week02/weighted_rozerin_yasar.py
@@ -1,8 +1,10 @@
-import random
-
-def weighted_srs(data, n, weights, with_replacement=False):
-    if weights:
-        return random.choices(data, weights=weights, k=n)
-    if with_replacement:
-        return random.choices(data, k=n)
-    return random.sample(data, n)
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    else:
+        result = []
+        while len(result) < n:
+            item = random.choices(data, weights=weights, k=1)[0]
+            if item not in result:
+                result.append(item)
+        return result
